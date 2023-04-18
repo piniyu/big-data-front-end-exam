1cc172054ce07252625b112a2eb8a13c65dc3b0d
Ching Chen <piniyu699@gmail.com>
2023-04-18 13:15:18 +0800

    chore: set up development environment
    - add eslint(airbnb style)
    - add prettier
    - converted js to ts
    
    chore: modified eslint config
    
    - modified eslint config to work with jest
    
    chore: update eslint and prettier settings
    
    - refactored eslint config to work with react router
    
    chore: add ignorepattern in eslint config
    
    - temperary ignore 'reportWebVitals.js' and 'vitals.js'
    
    chore: modified eslint config to work with jest
    
    - modified eslint config to work with jest

---
+++ src/App.test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react"
+import App from "./App"
+
+// test("renders learn react link", () => {
+//   render(<App />)
+//   const linkElement = screen.getByText(/learn react/i)
+//   expect(linkElement).toBeInTheDocument()
+// })
+test("renders header", () => {
+  render(<App />)
+  const headerElement = screen.getByText(/人口數、戶數按戶別及性別統計/i)
+
+  expect(headerElement).toBeInTheDocument()
+})
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import "./App.css"
+
+function App() {
+  return (
+    <div className="App">
+      <nav>LOGO</nav>
+      <header>人口數、戶數按戶別及性別統計</header>
+    </div>
+  )
+}
+
+export default App
